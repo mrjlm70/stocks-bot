72a8ffc83cd233f67ec69b85fb499a31c650fe17
Amirhosein <101968274+mrjlm70@users.noreply.github.com>
2025-05-11 12:19:20 -0500

    Update bursindex_api.py
    "

--- bursindex_api.py
+++ bursindex_api.py
@@ -10,7 +10,7 @@ response = requests.get(url)
 def get_bursindex():
     if response.status_code == 200:
         data = response.json()
-        emoji = "🟢📈" if data["index_change"] >= 0 else "🔴📉"
+        emoji = '🟢📈' if data["index_change"] >= 0 else '🔴📉'
         return f"<code>Tehran Stock Index: {float(data['index']):,.2f} {emoji} {float(data['index_change']):,.2f}</code>"
     else:
         print("خطا در دریافت داده:", response.status_code)
